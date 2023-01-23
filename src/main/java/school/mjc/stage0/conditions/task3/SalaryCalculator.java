5b48fc05ebbc8ef1c0f8b50307bfd4a24fd11b4b
Aziz1019 <starinc1998@gmail.com>
2023-01-23 11:34:08 +0500

    Initial Commit

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if(salary < 0) {
+            System.out.println("wrong input!");
+        }
+        else if(salary <= 10000){
+            double basicSalary = salary - (salary * 0.15);
+            System.out.println(basicSalary);
+        } else if (salary <= 20000) {
+            double basicSalary = salary - (salary * 0.18);
+            System.out.println(basicSalary);
+        } else if(salary > 20000) {
+            double basicSalary = salary - (salary * 0.20);
+            System.out.println(basicSalary);
+        }
+    }
+
+    public static void main(String[] args) {
+        SalaryCalculator calculator = new SalaryCalculator();
+        calculator.calculateSalary(-1);
     }
 }
